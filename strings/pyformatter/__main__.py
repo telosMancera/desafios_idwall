ece2ed4ce1c47e8de38be60661eca1b61f9c7dd0
Telos Mancera <telosgm@hotmail.com>
2022-05-07 23:20:07 -0300

    Added all parts to main

--- strings/pyformatter/__main__.py
+++ strings/pyformatter/__main__.py
@@ -1,4 +1,22 @@
+from pyformatter.formatter import StringFormatter
 from pyformatter.functions import parse_execution_arguments
 
-execution_arguments = parse_execution_arguments()
-print(execution_arguments)
+
+def main() -> None:
+    execution_arguments = parse_execution_arguments()
+    print(execution_arguments)
+    print("")
+
+    formatter = StringFormatter(execution_arguments["limit"])
+    formatted = formatter.format(
+        execution_arguments["text"], justify=execution_arguments["justify"]
+    )
+    print(formatted)
+
+    if execution_arguments["file"]:
+        with open(execution_arguments["file"], "w") as file:
+            file.write(formatted)
+
+
+if __name__ == "__main__":
+    main()
