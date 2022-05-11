db5b6cef69370550fc382551a63cea6ca69faa99
Telos Mancera <telosgm@hotmail.com>
2022-05-11 20:55:52 -0300

    Refactored crawler.crawlers.functions

--- crawlers/crawler/crawlers/functions.py
+++ crawlers/crawler/crawlers/functions.py
@@ -1,11 +1,21 @@
+"""
+Solution based in
+https://stackoverflow.com/questions/38785877/spoofing-ip-address-when-web-scraping-python
+"""
+
+
 import random
 from json import dumps, loads
 from os.path import exists
 from urllib.request import Request, urlopen
 
 from bs4 import BeautifulSoup
+from crawler.logs import get_logger
+from crawler.settings import SOUP_FEATURES
 from fake_useragent import UserAgent
 from IPython.display import clear_output
+
+logger = get_logger(__name__)
 
 USER_AGENT_LIST = (
     # Chrome
@@ -36,6 +46,8 @@ USER_AGENT_LIST = (
 )
 PROXIES_FILE = ".proxies"
 
+logger = get_logger(__name__)
+
 # Here I provide some proxies for not getting caught while scraping
 ua = UserAgent()  # From here we generate a random user agent
 proxies = []  # Will contain proxies [ip, port]
@@ -43,36 +55,73 @@ proxies = []  # Will contain proxies [ip, port]
 
 # Main function
 def generate_proxies() -> None:
-    global proxies
 
-    if exists(PROXIES_FILE):
-        try:
-            with open(PROXIES_FILE, "r") as file:
-                proxies = loads(file.read())
+    """
+    Generate proxies for crawling.
+    """
 
-        except Exception:
-            pass
+    logger.info("Generate proxies...")
 
-        else:
-            return
+    if _could_get_from_file():
+        logger.info("Proxies got from %s!", PROXIES_FILE)
 
+        return
+
+    logger.info("Couldn't get proxies from file! Generating a new one...")
+
+    _get_new_proxies()
+    # _delete_invalid_proxies()
+
+    with open(PROXIES_FILE, "w") as file:
+        file.write(dumps(proxies))
+
+    logger.info("Proxies saved at %s", PROXIES_FILE)
+
+
+def get_random_proxy() -> list:
+    return random.choice(proxies)
+
+
+def get_random_user_agent() -> str:
+    return random.choice(USER_AGENT_LIST)
+
+
+def _could_get_from_file() -> bool:
+    if not exists(PROXIES_FILE):
+        return False
+
+    try:
+        global proxies
+        with open(PROXIES_FILE, "r") as file:
+            proxies = loads(file.read())
+
+    except Exception:
+        return False
+
+    else:
+        return True
+
+
+def _get_new_proxies():
     # Retrieve latest proxies
-    # proxies_req = Request("https://www.sslproxies.org/")
-    # proxies_req.add_header("User-Agent", ua.random)
-    # proxies_doc = urlopen(proxies_req).read().decode("utf8")
+    proxies_req = Request("https://www.sslproxies.org/")
+    proxies_req.add_header("User-Agent", ua.random)
+    proxies_doc = urlopen(proxies_req).read().decode("utf8")
 
-    with open("sslproxies.org.html", "r") as file:
-        soup = BeautifulSoup(file, "html.parser")
-
+    soup = BeautifulSoup(proxies_doc, SOUP_FEATURES)
     proxies_table = BeautifulSoup(str(soup.find_all(id="list"))).find_all("table")[0]
 
     # Save proxies in the array
+    global proxies
     for row in proxies_table.tbody.find_all("tr"):
         proxies.append(
             {"ip": row.find_all("td")[0].string, "port": row.find_all("td")[1].string}
         )
 
+
+def _delete_invalid_proxies() -> None:
     # Choose a random proxy
+    global proxies
     proxy_index = _random_proxy()
     proxy = proxies[proxy_index]
 
@@ -97,18 +146,7 @@ def generate_proxies() -> None:
             proxy_index = _random_proxy()
             proxy = proxies[proxy_index]
 
-    with open(PROXIES_FILE, "w") as file:
-        file.write(dumps(proxies))
-
-
-def get_random_proxy() -> list:
-    return random.choice(proxies)
-
-
-def get_random_user_agent() -> str:
-    return random.choice(USER_AGENT_LIST)
-
 
 # Retrieve a random index proxy (we need the index to delete it if not working)
-def _random_proxy():
+def _random_proxy() -> int:
     return random.randint(0, len(proxies) - 1)
