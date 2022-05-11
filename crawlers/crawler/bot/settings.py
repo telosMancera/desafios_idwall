d1285f56d662c7b0d5e721cc0ec8e051eb2294b0
Telos Mancera <telosgm@hotmail.com>
2022-05-11 20:15:08 -0300

    Updated README for crawlers

--- crawlers/crawler/bot/settings.py
+++ crawlers/crawler/bot/settings.py
@@ -1,5 +1,3 @@
 from os import environ
 
-TELEGRAM_BOT_TOKEN = environ.get(
-    "TELEGRAM_BOT_TOKEN", "5336829044:AAGr8kYrme1zoYUjR7921D29VYHtXwGniIg"
-)
+TELEGRAM_BOT_TOKEN = environ.get("TELEGRAM_BOT_TOKEN")
